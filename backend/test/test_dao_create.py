62bd60f9b045b244c6709d5bff3de11866df9824
Vilhelm Malmberg Eskilsson <vima23@student.bth.se>
2025-04-24 12:22:15 +0200

    more tests for dao create

--- backend/test/test_dao_create.py
+++ backend/test/test_dao_create.py
@@ -3,14 +3,14 @@
 
 import os
 import pytest
-import json
 from src.util.dao import DAO
+from pymongo.errors import WriteError
 
 @pytest.fixture
 def connect_to_test_db():
     os.environ['MONGO_URL'] = 'mongodb://root:root@localhost:27017/'
 
-def test_create(connect_to_test_db):
+def test_create_valid_user(connect_to_test_db):
     test_dao = DAO('user')
     test_user = {
         'firstName': 'Hasse',
@@ -22,6 +22,62 @@ def test_create(connect_to_test_db):
     assert res['firstName'] == 'Hasse'
     assert res['lastName'] == 'Aro'
     assert res['email'] == 'aro@tv3.se'
+    test_dao.delete(res['_id']['$oid'])
+    expectNone = test_dao.findOne(res.get('_id')['$oid'])
+    assert expectNone == None
+
+def test_create_invalid_user_no_first_name(connect_to_test_db):
+    test_dao = DAO('user')
+    test_user = {
+        'lastName': 'Aro',
+        'email': 'aro@tv3.se'
+    }
+    with pytest.raises(Exception) as exc_info:
+        test_dao.create(test_user)
+    assert exc_info.type == WriteError
+
+def test_create_invalid_user_no_last_name(connect_to_test_db):
+    test_dao = DAO('user')
+    test_user = {
+        'firstName': 'Hasse',
+        'email': 'aro@tv3.se'
+    }
+    with pytest.raises(Exception) as exc_info:
+        test_dao.create(test_user)
+    assert exc_info.type == WriteError
+
+def test_create_invalid_user_no_last_name(connect_to_test_db):
+    test_dao = DAO('user')
+    test_user = {
+        'firstName': 'Hasse',
+    }
+    with pytest.raises(Exception) as exc_info:
+        test_dao.create(test_user)
+    assert exc_info.type == WriteError
+    
+def test_create_user_not_unique(connect_to_test_db):
+    test_dao = DAO('user')
+    test_user = {
+        'firstName': 'Hasse',
+        'lastName': 'Aro',
+        'email': 'aro@tv3.se'
+    }
+    res = test_dao.create(test_user)
+    assert isinstance(res, dict)
+    assert res['firstName'] == 'Hasse'
+    assert res['lastName'] == 'Aro'
+    assert res['email'] == 'aro@tv3.se'
+    
+    second_user = {
+        'firstName': 'Hans',
+        'lastName': 'Aroldsson',
+        'email': 'aro@tv3.se'
+    }
+
+    with pytest.raises(Exception) as exc_info:
+        test_dao.create(second_user)
+    assert exc_info.type == WriteError
+
     test_dao.delete(res['_id']['$oid'])
     expectNone = test_dao.findOne(res.get('_id')['$oid'])
     assert expectNone == None
