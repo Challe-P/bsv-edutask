d739a0125e38f7b83775015af01d9cc9aed744bb
Vilhelm Malmberg Eskilsson <vima23@student.bth.se>
2025-05-12 12:22:35 +0200

    Fixes so that only unit tests run

--- backend/test/test_usercontroller.py
+++ backend/test/test_usercontroller.py
@@ -2,6 +2,7 @@ import pytest
 import unittest.mock as mock
 from src.controllers.usercontroller import UserController
 
+@pytest.mark.unittest
 def test_get_user_by_email_valid_unique():
     expected_result = {'id': 'user1', 'email': 'unique@test.com'}
     mocked_db = mock.MagicMock()
@@ -10,6 +11,7 @@ def test_get_user_by_email_valid_unique():
     res = controller.get_user_by_email('unique@test.com')
     assert res == expected_result
 
+@pytest.mark.unittest
 def test_get_user_by_email_valid_not_unique(capfd):
     expected_result = {'id': 'user1', 'email': 'not_unique@test.com'}
     mocked_db = mock.MagicMock()
@@ -20,6 +22,7 @@ def test_get_user_by_email_valid_not_unique(capfd):
     assert captured.out == 'Error: more than one user found with mail not_unique@test.com\n'
     assert res == expected_result
 
+@pytest.mark.unittest
 def test_get_user_by_email_valid_no_user():
     expected_result = None
     mocked_db = mock.MagicMock()
@@ -28,6 +31,7 @@ def test_get_user_by_email_valid_no_user():
     res = controller.get_user_by_email('unique@test.com')
     assert res == expected_result
 
+@pytest.mark.unittest
 def test_get_user_db_failure():
     mocked_db = mock.MagicMock()
     mocked_db.find = mock.Mock(side_effect=Exception)
@@ -36,6 +40,7 @@ def test_get_user_db_failure():
         controller.get_user_by_email('unique@test.com')
     assert exc_info.type == Exception
 
+@pytest.mark.unittest
 @pytest.mark.parametrize('email', ['example@test.com', 'common.format@test.com', 'FirstName.LastName@TestTest.com', 
     'x@test.com', 'long-local-with-hyphens@and.subdomains.com', 'some.name+tag+tag@test.com',
     'sur/name@test.com', 'admin@test', '" "@test.com', '"very.(),:;<>[]\".VERY.\"very@\\ \"very\".strange"@unusual.test.com'])
@@ -47,6 +52,7 @@ def test_valid_email(email):
     res = controller.get_user_by_email(email)
     assert res == expected_result
 
+@pytest.mark.unittest
 @pytest.mark.parametrize('email', ['abc.test.com', 'a@b@c@test.com', '123', 't"o(o)s,p:e;c<i>a[l\k]l@test.com', 'no"quotes"allowed@test.com',
     'no spaces"and\quotes@test.com', 'even\ escaped\"chars\\arent@test.com', '1234567890123456789012345678901234567890123456789012345678901234+x@test.com',
     'no.underscores@are_allowed_here.com'])
