f36129a0e09614d12d4cfdac4a1548f20f37a201
Vilhelm Malmberg Eskilsson <vima23@student.bth.se>
2025-05-13 11:53:41 +0200

    Changes viewport for cypress

--- frontend/cypress.config.js
+++ frontend/cypress.config.js
@@ -2,6 +2,8 @@ const { defineConfig } = require("cypress");
 require('dotenv').config();
 
 module.exports = defineConfig({
+  viewportHeight: 800,
+  viewportWidth: 1280,
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
